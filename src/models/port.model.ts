0ec0083d1ef0831bfe6cd9f378ae22e616cca3f8
Navid <navidtaheri@gmail.com>
2024-12-21 10:51:58 +0100

    port model is updated.

--- src/models/port.model.ts
+++ src/models/port.model.ts
@@ -1,9 +1,14 @@
-import mongoose from "mongoose";
+import mongoose, { Schema, Document } from "mongoose";
 
-const portSchema = new mongoose.Schema({
-  name: { type: String, required: true },
+// TypeScript interface for Port document
+export interface IPort extends Document {
+  name: string;
+}
+
+// Define the Mongoose schema
+const PortSchema: Schema = new Schema({
+  name: { type: String, required: true, unique: true }, // Ensure name is unique
 });
 
-const Port = mongoose.model("Port", portSchema);
-
-export default Port;
+// Export the Mongoose model with TypeScript typing
+export default mongoose.model<IPort>("Port", PortSchema);
